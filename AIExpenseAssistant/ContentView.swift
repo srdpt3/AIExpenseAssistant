84993eaa55749c49109d37cb0dd24d9470aefde7
Dustin Yang <srdpt3@gmail.com>
2024-08-11 21:42:22 -0400

    LogFormView

--- AIExpenseAssistant/ContentView.swift
+++ AIExpenseAssistant/ContentView.swift
@@ -12,14 +12,67 @@ struct ContentView: View {
     @State var vm = LogListViewModel()
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        #if os(macOS)
+        splitView
+        #elseif os(visionOS)
+        tabView
+        #else
+        switch horizontalSizeClass {
+        case .compact: tabView
+        default: splitView
         }
-        .padding()
+        #endif
     }
+    
+    var tabView: some View {
+          TabView {
+              NavigationStack {
+                  LogListContainerView(vm: $vm)
+              }
+              .tabItem {
+                  Label("지출", systemImage: "tray")
+              }.tag(0)
+              
+              NavigationStack {
+                  Text("AI Assistant")
+//                  AIAssistantView()
+              }
+              .tabItem {
+                  Label("AI 비서", systemImage: "waveform")
+              }.tag(1)
+              
+              NavigationStack {
+                  Text("영수증 스캔")
+
+//                  ExpenseReceiptScannerView()
+              }
+              .tabItem {
+                  Label("영수증 스캔", systemImage: "eye")
+              }.tag(2)
+          }
+      }
+      
+      var splitView: some View {
+          NavigationSplitView {
+              List {
+                  NavigationLink(destination: LogListContainerView(vm: $vm)) {
+                      Label("Expenses", systemImage: "tray")
+                  }
+                  
+                  NavigationLink(destination: Text("AI 비서")) {
+                      Label("AI Assistant", systemImage: "waveform")
+                  }
+                  
+                  NavigationLink(destination: Text("영수증 스캔")) {
+                      Label("Receipt Scanner", systemImage: "eye")
+                  }
+                  
+              }
+          } detail: {
+              LogListContainerView(vm: $vm)
+          }
+          .navigationTitle("XCA AI Expense Tracker")
+      }
 }
 
 #Preview {
